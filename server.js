a5da2143db3fb4ddc038f2de522b88e61a98e57f
AustinT33 <austintumlinson@gmail.com>
2020-03-02 20:20:04 -0600

    testing cors

--- server.js
+++ server.js
@@ -5,12 +5,10 @@ const catRoute = require('./src/cat-router');
 const {CLIENT_ORIGIN} = require('./src/config')
 const app = express();
 
-app.use(cors({
-  origin: CLIENT_ORIGIN
-}));
+app.use(cors());
 
-app.use('/api/dog', cors(), dogRoute);
-app.use('/api/cat', cors(), catRoute);
+app.use('/api/dog', dogRoute);
+app.use('/api/cat', catRoute);
 
 // Catch-all 404
 app.use(function (req, res, next) {
